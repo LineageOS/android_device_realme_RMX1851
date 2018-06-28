c6d8f75ba1b91313dab687ed18ed8a431600f7e4
Christian Oder <myself5@carbonrom.org>
2018-06-27 21:14:43 -0400

    RMX1851: Build disabled VBMeta image
    Change-Id: I381feef8f6fefc8449ca50d85d704b67bcc8a77e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,5 +135,9 @@ BOARD_VNDK_RUNTIME_DISABLE := true
 BOARD_VNDK_VERSION := current
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+
 # Inherit from the proprietary version
 -include vendor/realme/RMX1851/BoardConfigVendor.mk
