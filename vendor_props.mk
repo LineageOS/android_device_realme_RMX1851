421aa4d9d6f23d837a9aa34a8bd51587a65cdef6
Richa Agarwal <richagar@codeaurora.org>
2018-11-26 15:51:31 +0530

    RMX1851: Changes for perf 2.0
    Moving from perf-hal 1.0 to 2.0
    
    Change-Id: I962cebaa3f4b9641439bcce8d7bd0933b2d93cb9

--- vendor_props.mk
+++ vendor_props.mk
@@ -114,8 +114,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.core_ctl_min_cpu=4 \
-    ro.vendor.qti.core_ctl_max_cpu=6 \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60
+    ro.vendor.qti.core_ctl_max_cpu=6
 
 # RCS and IMS
 PRODUCT_PROPERTY_OVERRIDES += \
