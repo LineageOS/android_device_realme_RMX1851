49f091f6f1fcae591c8ec3b15379fde10e1a618e
Ethan Chen <intervigil@gmail.com>
2019-02-03 15:12:39 -0800

    RMX1851: Disable verity and descriptor checking
    * avbtool takes a --flags argument, not --flag
    * avbtool provides an option to disable hashtree checking
    
    Change-Id: Ib57d8ebed69a0998005563a9268f9fc736cbeaf7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,6 +137,7 @@ PRODUCT_FULL_TREBLE_OVERRIDE := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
 # Inherit from the proprietary version
