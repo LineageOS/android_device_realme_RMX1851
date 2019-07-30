a84fdbb783cdb074f6215b6621a4e22d40e8ca3f
Bruno Martins <bgcngm@gmail.com>
2019-07-30 14:21:52 +0100

    RMX1851: Compile OMX from source
    Change-Id: I0e55d49e96423a9473059cf9000b0114b903d277

--- device.mk
+++ device.mk
@@ -197,6 +197,18 @@ PRODUCT_PACKAGES += \
     libnfnetlink
 
 # Media
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_oppo_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_oppo_performance.xml \
