0372ca2af6cbca76fbdbb901c572773350ae6111
Bruno Martins <bgcngm@gmail.com>
2019-08-02 14:13:42 +0100

    RMX1851: Also compile system variant of vendor.display.config
     * WFD service needs access to these shared libraries,
       which were only available in vendor partition so far.
       The access to the vendor variant is denied as per treble
       rules, so ship the system variant too. While at it,
       go with the latest version available.
    
    Change-Id: I8959a5a39a45ccf306f2a809789d29b30f4019ce

--- device.mk
+++ device.mk
@@ -155,7 +155,8 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libtinyxml \
     libvulkan \
-    vendor.display.config@1.7
+    vendor.display.config@1.7 \
+    vendor.display.config@1.7.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
