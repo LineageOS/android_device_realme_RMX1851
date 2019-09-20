b02834ac6aae90dcce142b6465eb9d6dcf897171
karthick111 <karthickrko61@gmail.com>
2019-09-20 11:11:32 +0530

    RMX1851: Add device specific keylayouts
    Change-Id: I66ad0442f0b44c5de53e46af6dbb958840c820a1

--- device.mk
+++ device.mk
@@ -81,7 +81,8 @@ PRODUCT_PACKAGES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/sdm845-tavil-snd-card_Button_Jack.kl:system/usr/keylayout/sdm845-tavil-snd-card_Button_Jack.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:/system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/sdm670-mtp-snd-card_Button_Jack.kl:system/usr/keylayout/sdm670-mtp-snd-card_Button_Jack.kl
 
 # Lights
 PRODUCT_PACKAGES += \
