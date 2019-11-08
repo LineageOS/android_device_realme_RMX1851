0c6ab6a7ccb2075f8068ce4ab0a17977804e013f
karthick111 <karthickrko61@gmail.com>
2019-11-08 13:22:37 +0530

    Revert "RMX1851: Switch to more generic dt2w node"
    This reverts commit cdd2d459d541513161a8d861bc1e6ee7423c69fb.
    
    Signed-off-by: karthick111 <karthickrko61@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,6 +107,9 @@ TARGET_USES_MKE2FS := true
 
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+
 # QCOM
 #BOARD_USES_QCOM_HARDWARE := true
 
