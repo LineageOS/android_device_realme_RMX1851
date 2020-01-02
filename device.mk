a3544b04053e0f24ed143089fbf3548bfc67e61d
karthick111 <karthickrko61@gmail.com>
2020-01-02 08:14:34 +0530

    RMX1851: sensors: Convert to RMX1851 target
    Change-Id: If257ec5501ef8245d098ffcf20eca41094388f0f

--- device.mk
+++ device.mk
@@ -287,7 +287,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-service.RMX1851 \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
