3418895d73c2eb27060dc6a2e24a1f778d369484
Bruno Martins <bgcngm@gmail.com>
2020-01-27 23:19:59 +0000

    RMX1851: Build missing WiFi Display libs
    Change-Id: I96e83b50d7581d4d74ade83451418b71989f7a64

--- device.mk
+++ device.mk
@@ -357,7 +357,9 @@ PRODUCT_COPY_FILES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
+    libdisplayconfig.vendor \
     libnl \
+    libqdMetaData \
     libqdMetaData.system
 
 PRODUCT_BOOT_JARS += \
