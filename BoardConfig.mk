97b335c36470440cff666616e6eb15cd85de2493
karthick111 <karthickrko61@gmail.com>
2020-06-09 09:12:41 +0530

    RMX1851: Split system, odm and vendor props
    Change-Id: Idbd82aeeafd67a6fd5ef8775c5214ba686a91f48

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,9 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
--- device.mk
+++ device.mk
@@ -80,9 +80,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Properties
--include $(LOCAL_PATH)/vendor_props.mk
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
